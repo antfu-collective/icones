79997aa89925c56f4ed93e376eaac80dfa4f2fe2
Anthony Fu <anthonyfu117@hotmail.com>
2020-08-05 13:28:15 +0800

    fix: JSZIP undefiend, resolve #6

---
+++ scripts/postbuild.ts
@@ -0,0 +1,7 @@
+import { promises as fs } from 'fs'
+
+;(async() => {
+  const path = './dist/index.html'
+  const file = await fs.readFile(path, 'utf-8')
+  await fs.writeFile(path, file.replace(/\/\/lib/g, '/lib'), 'utf-8')
+})()
