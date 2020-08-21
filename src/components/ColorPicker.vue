a072754e8978118901bdd7d783b125472a41e391
Anthony Fu <anthonyfu117@hotmail.com>
2020-08-21 22:55:45 +0800

    chore: update linter

--- src/components/ColorPicker.vue
+++ src/components/ColorPicker.vue
@@ -13,14 +13,14 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue'
 
 export default defineComponent({
   props: {
     value: {
       type: String,
-      required: true
-    }
-  } 
+      required: true,
+    },
+  },
 })
 </script>
--- src/window.d.ts
+++ src/window.d.ts
@@ -1,6 +1,8 @@
 import type JSZip from 'jszip'
 
-declare interface Window {
-  JSZip: JSZip
-  SvgPacker: (options: any) => Promise<any>
+declare global {
+  declare interface Window {
+    JSZip: JSZip
+    SvgPacker: (options: any) => Promise<any>
+  }
 }
