ecd492763b2b2556b4b0b390c0437a5069c70300
Anthony Fu <anthonyfu117@hotmail.com>
2020-10-27 14:52:54 +0800

    feat: PWA!!!

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { UserConfig } from 'vite'
 import Voie from 'vite-plugin-voie'
 import PurgeIcons from 'vite-plugin-purge-icons'
 import ViteComponents from 'vite-plugin-components'
+import { VitePWA } from 'vite-plugin-pwa'
 import dayjs from 'dayjs'
 
 const config: UserConfig = {
@@ -11,6 +12,24 @@ const config: UserConfig = {
     }),
     ViteComponents(),
     PurgeIcons(),
+    VitePWA({
+      manifest: {
+        name: 'Icônes',
+        short_name: 'Icônes',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
   ],
   define: {
     __BUILD_TIME__: JSON.stringify(dayjs().format('YYYY/MM/DD HH:mm')),
