356962856359bfb319a493cb16d96f09171e61cf
Anthony Fu <anthonyfu117@hotmail.com>
2021-03-30 21:33:07 +0800

    chore: update deps

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from 'voie-pages'
 import App from './App.vue'
+import 'virtual:windi.css'
+import 'virtual:windi-devtools'
 import './utils/electron'
-import 'windi.css'
 import './main.css'
 
 // import icons data genereted by PurgeIcons
