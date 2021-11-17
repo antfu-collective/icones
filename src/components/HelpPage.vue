3df9317ba0463e64d519ac15e4185cbe4b88e963
Anthony Fu <anthonyfu117@hotmail.com>
2021-11-18 02:22:53 +0800

    docs: icon usage

--- src/components/HelpPage.vue
+++ src/components/HelpPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-6 w-110 help-page">
+  <div class="p-6 w-120 help-page">
     <p class="mb-2 opacity-75">
       How to use the icon?
     </p>
@@ -13,17 +13,24 @@
     </p>
 
     <h2 class="bold text-lg mt-5 mb-1">
-      Build tools
+      Components
     </h2>
     <p>
       Created by the author of <b>Icônes</b>, <a href="https://github.com/antfu/unplugin-icons" target="_blank"><code>unplugin-icons</code></a> is a band new
       solution to use generate icons on-demand as components.
     </p>
     <p class="mt-2">
-      It works prefectly for any frameworks and any build tools, while providing the excellent DX without sarifising the UX on the user-side.
+      Check out <a href="https://antfu.me/posts/journey-with-icons-continues" target="_blank">this blog post</a> for the story behind.
+    </p>
+
+    <h2 class="bold text-lg mt-5 mb-1">
+      Pure CSS
+    </h2>
+    <p>
+      With the power of <a href="https://github.com/antfu/unocss">UnoCSS</a>, you can also use the icons with <b>Pure CSS</b> using <a href="https://github.com/antfu/unocss/tree/main/packages/preset-icons" target="_blank"><code>@unocss/preset-icons</code></a>.
     </p>
     <p class="mt-2">
-      Check out <a href="https://antfu.me/posts/journey-with-icons-continues" target="_blank">this blog post</a> for more the story behind.
+      Check out <a href="https://antfu.me/posts/icons-in-pure-css" target="_blank">this blog post</a> for more.
     </p>
   </div>
 </template>
