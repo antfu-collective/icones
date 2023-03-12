5ce4c347ee5df17f37739cf6aa772aa47c760d89
Anthony Fu <anthonyfu117@hotmail.com>
2023-03-12 12:26:47 +0100

    feat: support search alias, close #105, close #107

---
+++ src/data/search-alias.ts
@@ -0,0 +1,11 @@
+export const searchAlias: string[][] = [
+  ['cog', 'gear', 'setting', 'adjust'],
+  ['close', 'cross'],
+  ['person', 'account', 'people', 'user'],
+  ['plus', 'add'],
+  ['minus', 'remove'],
+  ['trash', 'delete'],
+  ['check', 'tick', 'done'],
+  ['arrow', 'direction'],
+  ['globe', 'global', 'earth', 'world'],
+]
