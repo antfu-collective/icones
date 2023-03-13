56b03415120f7c3edf7791760e3ba7a04145d2fa
jaw <2135326728@qq.com>
2023-03-13 09:40:02 +0800

    refactor: temporary remove tray

--- electron/src/main/index.ts
+++ electron/src/main/index.ts
@@ -1,81 +1,12 @@
 import path from 'path'
-import { BrowserWindow, Menu, Tray, app, dialog, nativeImage, shell } from 'electron'
+import { BrowserWindow, app, shell } from 'electron'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
-import Store from 'electron-store'
-import { showNotification } from './utils'
 
-const store = new Store()
 let mainWindow: BrowserWindow | null = null
-let tray: Tray | null = null
-let quitting = false
-
-enum ELECTRON_STORE {
-  /** Ask before exiting the app */
-  close_behavior = 'close_behavior',
-}
-
-enum CLOSE_BEHAVIOR {
-  quit,
-  /** Minimize to tray */
-  tray,
-}
 
 app.disableHardwareAcceleration()
 
 const PROJECT_ROOT = path.resolve(__dirname, '../..')
-
-const exitApp = () => {
-  quitting = true
-  app.quit()
-}
-
-const promptUserToExit = async (win: BrowserWindow) => {
-  const { response, checkboxChecked } = await dialog.showMessageBox(win, {
-    type: 'question',
-    message: 'Do you want to quit or minimize to tray?',
-    buttons: ['Quit and close', 'Minimize to tray'],
-    checkboxLabel: 'Remember my choice',
-    cancelId: 2,
-  })
-
-  if (checkboxChecked)
-    store.set(ELECTRON_STORE.close_behavior, response)
-
-  if (response === CLOSE_BEHAVIOR.tray)
-    win.hide()
-  else if (response === CLOSE_BEHAVIOR.quit)
-    exitApp()
-}
-
-const clearStorage = async (win: BrowserWindow) => {
-  await win.webContents.session.clearStorageData()
-  win.reload()
-  showNotification('Clear successfully')
-}
-
-const clearSetting = () => {
-  store.delete(ELECTRON_STORE.close_behavior)
-  showNotification('Reset successfully')
-}
-
-const createTray = (win: BrowserWindow) => {
-  const iconPath = path.resolve(PROJECT_ROOT, app.isPackaged ? '../icon.png' : 'build/icon.png')
-  const icon = nativeImage.createFromPath(iconPath)
-  tray = new Tray(icon)
-
-  const contextMenu = Menu.buildFromTemplate([
-    { label: 'Open App', click: () => win.show() },
-    { type: 'separator' },
-    { label: 'Clear User Data', click: () => clearStorage(win) },
-    { label: 'Reset Settings', click: () => clearSetting() },
-    { type: 'separator' },
-    { label: 'Quit', click: () => exitApp() },
-  ])
-
-  tray.setContextMenu(contextMenu)
-  tray.setToolTip(app.name)
-  tray.on('click', () => win.show())
-}
 
 const createMainWindow = async () => {
   const win = new BrowserWindow({
@@ -104,20 +35,6 @@ const createMainWindow = async () => {
 
   win.on('ready-to-show', () => {
     win.show()
-  })
-
-  win.on('close', async (e) => {
-    const closeBehavior = store.get(ELECTRON_STORE.close_behavior) as CLOSE_BEHAVIOR | undefined
-
-    if (!quitting) {
-      e.preventDefault()
-      if (typeof closeBehavior !== 'number')
-        await promptUserToExit(win)
-      else if (closeBehavior === CLOSE_BEHAVIOR.tray)
-        win.hide()
-      else
-        exitApp()
-    }
   })
 
   win.on('closed', () => {
@@ -157,7 +74,6 @@ app.on('activate', async () => {
   await app.whenReady()
 
   mainWindow = await createMainWindow()
-  createTray(mainWindow)
   mainWindow.focus()
 })()
   .catch(console.error)
--- electron/src/main/utils.ts
+++
@@ -1,5 +0,0 @@
-import { Notification, app } from 'electron'
-
-export const showNotification = (mes: string) => {
-  new Notification({ title: app.name, body: mes }).show()
-}
