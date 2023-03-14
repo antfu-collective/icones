8cf5f770fc9573f79b6be8fe8fa979ce0772f616
jaw <2135326728@qq.com>
2023-03-14 17:45:26 +0800

    ui: adjust select size

--- src/components/CustomSelect.vue
+++ src/components/CustomSelect.vue
@@ -22,16 +22,19 @@ onClickOutside(target, () => visible.value = false)
     <div @click="visible = true">
       <slot />
     </div>
-    <div v-if="visible" class="absolute rounded-md w-60 border-gray-300 border-1 dark:bg-dark-100 text-base top-20px right-0 z-10 shadow-md text-gray-500 px-6 py-3 bg-base">
+    <div
+      v-if="visible"
+      class="absolute rounded-md w-60 border-gray-300 border-1 dark:bg-dark-100 text-base top-20px right-0 z-10 shadow-md text-gray-500 px-4 py-2 bg-base"
+    >
       <template v-for="(optgroup) in options" :key="optgroup.label">
-        <div text-gray-600 font-semibold py-2>
+        <div text-gray-600 font-semibold py-1>
           {{ optgroup.label }}
         </div>
 
         <div
           v-for="(option) in optgroup.children"
           :key="option.value"
-          class="cursor-pointer mx-2 text-sm leading-6 py-2 pl-4 dark:hover-bg-gray-800 hover-bg-gray-100 hover-rounded"
+          class="cursor-pointer mx-2 text-sm leading-6 py-1 pl-2 dark:hover-bg-gray-800 hover-bg-gray-100 hover-rounded"
           :class="{
             'color-primary': option.value === modelValue,
             'cursor-not-allowed opacity-50': option?.disabled,
