ebab42b30e4bca3bff7618636fe1d2ba23c1759f
userquin <userquin@gmail.com>
2023-04-22 02:06:27 +0200

    feat: open caches and add fallback on sw interception

--- src/sw.ts
+++ src/sw.ts
@@ -1,5 +1,5 @@
 import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
-import { clientsClaim } from 'workbox-core'
+import { cacheNames, clientsClaim } from 'workbox-core'
 import { NavigationRoute, registerRoute } from 'workbox-routing'
 import { getIcons } from '@iconify/utils'
 
@@ -19,23 +19,31 @@ registerRoute(new NavigationRoute(
 self.skipWaiting()
 clientsClaim()
 
+function openCaches(e: FetchEvent) {
+  e.waitUntil(caches.open(cacheNames.precache))
+  return Promise.resolve()
+}
+
 self.addEventListener('fetch', (e) => {
   const url = e.request.url
   const match = url.match(/^https:\/\/(api\.iconify\.design|api\.simplesvg\.com|api\.unisvg\.com)\/(.*)\.json\?icons=(.*)?/)
   if (match) {
     e.respondWith(
-      fetch(`/collections/${match[2]}-raw.json`)
-        .then((response) => {
-          return response.json()
-        }).then((collection) => {
-          return new Response(JSON.stringify(getIcons(
-            collection,
-            match[3].replaceAll('%2C', ',').split(','),
-          )), {
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          })
+      openCaches(e)
+        .then(() => {
+          return fetch(`/collections/${match[2]}-raw.json`)
+            .then(response => response.json())
+            .then((collection) => {
+              return new Response(JSON.stringify(getIcons(
+                collection,
+                match[3].replaceAll('%2C', ',').split(','),
+              )), {
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+              })
+            })
+            .catch(() => fetch(e.request))
         }),
     )
   }
