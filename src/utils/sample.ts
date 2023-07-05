596f2229afedcd18b86c8d57293d83a69568eb66
Anthony Fu <anthonyfu117@hotmail.com>
2023-07-05 15:27:16 +0200

    chore: update

--- src/utils/sample.ts
+++ src/utils/sample.ts
@@ -1,8 +1,3 @@
 export function sample<T>(arr: T[], num: number) {
-  const result = new Array<T>(num)
-
-  for (let i = 0; i < num; i++)
-    result[i] = arr[Math.floor(arr.length * Math.random())]
-
-  return result
+  return Array.from({ length: num }, () => arr[Math.floor(arr.length * Math.random())])
 }
