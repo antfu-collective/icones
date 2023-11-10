81c40caa5f8e885bcdbede77a1bce16fbccdbe2f
Anthony Fu <anthonyfu117@hotmail.com>
2023-11-10 13:21:53 +0100

    chore: fix

--- scripts/prepare.ts
+++ scripts/prepare.ts
@@ -1,6 +1,7 @@
 import path from 'node:path'
 import fs from 'fs-extra'
 
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
 const out = path.resolve(__dirname, '../public')
 
 function ObjectPick(source: Record<string, any>, keys: string[]) {
